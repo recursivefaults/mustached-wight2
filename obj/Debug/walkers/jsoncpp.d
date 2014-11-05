14efa7469771217281b221dfc5cad9a170f0c112
Ryan Latta <ryan.latta@appia.com>
2014-11-05 18:47:50 -0500

    Initial Commit

---
+++ obj/Debug/walkers/jsoncpp.d
@@ -0,0 +1,3 @@
+obj/Debug/walkers/jsoncpp.o: lib/jsoncpp.cpp lib/json/json.h
+
+lib/json/json.h:
---
+++ premake4.lua
@@ -0,0 +1,27 @@
+-- A solution contains projects, and defines the available configurations
+solution "Walkers"
+   configurations { "Debug", "Release" }
+   includedirs {"/usr/local/include", "include", "lib"}
+   links {"SDL2", "SDL2_image" }
+   libdirs {"/usr/local/lib", "lib"}
+   kind "ConsoleApp"
+   buildoptions {"-Wall", "-std=c++11" }
+   language "C++"
+ 
+   -- A project defines one build target
+   project "walkers"
+      files { "src/**.h", "src/**.cpp", "lib/jsoncpp.cpp" }
+      excludes { "test/*" }
+ 
+      configuration "Debug"
+         defines { "DEBUG" }
+         flags { "Symbols" }
+ 
+      configuration "Release"
+         defines { "NDEBUG" }
+         flags { "Optimize" }
+
+    project "test"
+        excludes { "src/main.cpp" }
+        files { "src/**.h", "src/**.cpp", "lib/jsoncpp.cpp", "test/**.cpp" }
+        includedirs {"/usr/local/include", "lib"}
